aa069c1e3758d2bd4997343e57e6110c0fc27312
yogitaAP <yogita088sharma@gmail.com>
2017-03-09 00:15:29 +0530

    intermediate commit

--- quiz-collection/lesson-17-quiz/quiz-1.py
+++ quiz-collection/lesson-17-quiz/quiz-1.py
@@ -6,7 +6,8 @@ import tensorflow as tf
 def run():
     output = None
     x = tf.placeholder(tf.int32)
-    
+   
+   #testing the session run
     with tf.Session() as sess:
         output = sess.run(x, feed_dict={x: 123})
     return output
